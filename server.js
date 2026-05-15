1ede873a3ddc3b5f213e52fc5a028c61c761db27
Yang Susie Zhang <yszhang@umd.edu>
2026-05-15 14:48:34 -0400

    Add Render start script

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ app.post("/analyze-food", async (req, res) => {
 
 app.use("/", entriesRouter);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
